e29d51ccc4815e5b3784ea6287d8fe07d9aad404
CllDMR-Sargetek <celal.demir@sargetek.com>
2022-07-16 19:26:16 +0300

    implemented tailwind css

---
+++ apps/web/core/blog/tailwind.config.js
@@ -0,0 +1,14 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx,mdx}'),
+    ...createGlobPatternsForDependencies(join(__dirname, 'apps/web/core/blog')),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
