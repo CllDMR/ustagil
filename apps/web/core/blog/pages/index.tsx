dcd6cc30224c236a350fbfe960cf8f06a03c4a15
CllDMR-Sargetek <celal.demir@sargetek.com>
2022-07-16 22:37:09 +0300

    implemented common components to index-page

--- apps/web/core/blog/pages/index.tsx
+++ apps/web/core/blog/pages/index.tsx
@@ -1,8 +1,26 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
+import Footer from '../components/PageFooter/Footer';
+import Header from '../components/PageHeader/Header';
+import Main from '../components/PageMain/Main';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {};
 
-const IndexPage: NextPage<Props> = () => <div></div>;
+const IndexPage: NextPage<Props> = () => (
+  <>
+    <Head>
+      <title>Index Page</title>
+    </Head>
+
+    <Header />
+
+    <Main>
+      <article>Index Page</article>
+    </Main>
+
+    <Footer />
+  </>
+);
 
 export default IndexPage;
