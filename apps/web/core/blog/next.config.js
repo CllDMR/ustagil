4330f6ef1244e9201d9bf4f52db546fcf65bb1ce
CllDMR-Sargetek <celal.demir@sargetek.com>
2022-07-16 18:41:48 +0300

    implemented next mdx plugins

--- apps/web/core/blog/next.config.js
+++ apps/web/core/blog/next.config.js
@@ -1,5 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPlugins = require('next-compose-plugins');
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -12,4 +21,9 @@ const nextConfig = {
   },
 };
 
-module.exports = withNx(nextConfig);
+const mdxConfig = {
+  // Append the default value with md extensions
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+};
+
+module.exports = withPlugins([[withNx, nextConfig], [withMDX, mdxConfig]]);
