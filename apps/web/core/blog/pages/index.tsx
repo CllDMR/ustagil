f3b0bba020a87486e52d46dbc12820ee54c83d4e
CllDMR-Sargetek <celal.demir@sargetek.com>
2022-07-16 21:11:12 +0300

    moved to about and generated blank index page

---
+++ apps/web/core/blog/pages/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {};
+
+const IndexPage: NextPage<Props> = () => <div></div>;
+
+export default IndexPage;
